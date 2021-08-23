eae6b2697e6a099092e3b2b3619f0fdc0e5de9ab
Manojmm98 <manojmishra98feb@gmail.com>
2021-08-23 16:28:14 +0530

    updated till shallow and deep copy

--- SallowCopyVsDeepCopy/shallowVsdeep.js
+++ SallowCopyVsDeepCopy/shallowVsdeep.js
@@ -29,3 +29,26 @@ console.log(copyobj);
 let deepcopy= JSON.parse(JSON.stringify(obj));
 
 console.log(deepcopy);
+
+
+
+// if we want to copy the objects of obj1 for shallowcopy
+
+let obj1 = [{name:'manoj'}];
+
+let copyobj1=[];
+
+for(let i=0;i<obj1.length;i++) {
+    copyobj1.push(obj1[i]);
+}
+
+console.log(copyobj1);
+
+
+// for deep copy
+
+
+let deepcopy1=JSON.parse(JSON.stringify(copyobj1));
+
+console.log(deepcopy1);
+
